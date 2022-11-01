ff491d97fb3402c45fadcc58b09681d268446365
MAC <mac@miras-MBP.lan>
2022-11-01 21:47:41 +0100

    kommentarer

--- src/js/main.js
+++ src/js/main.js
@@ -27,41 +27,44 @@ for (i=0;i<toDoList.length;i++){
     let newLi = document.createElement("li");
     newLi.className="todoList"
     let newInput = document.createElement("input")
-    newInput.setAttribute("type","checkbox");
+    newInput.setAttribute("type","checkbox"); //gör input till en checkbox
 
     newLi.innerHTML+=toDoList[i].things;
-    let hej = toDoList[i];
+    let theList = toDoList[i]; //skapar en variabel till min lista som loopas
     newLi.appendChild(newInput); 
     newUl.appendChild(newLi);
     myDiv.appendChild(newUl);
-    newLi.addEventListener("click", ()=>{
-        myCheckbox(newInput, newLi, hej)}); 
+    newLi.addEventListener("click", ()=>{ 
+        myCheckbox(newInput, newLi, theList)}); //när jag klickar skickas dessa 3 värden till min function myCheckBox()
         
         
 
 } 
-console.log(toDoList)
+
 }
 
+console.log(toDoList) //skriver mina objekt. Här syns även att done:false
 
-function myCheckbox(myInput, newLi, myList) {
-    console.log(myList)
+function myCheckbox(myInput, newLi, clickedItem) {
     
-    if (myInput.checked===true) {
-        let index = toDoList.indexOf(myList);
-        newLi.innerHTML = " ";
+    
+    if (myInput.checked) {
+        let index = toDoList.indexOf(clickedItem);
+        newLi.innerHTML = ""; //gör att listan blir tom och försvinner
         toDoList.splice(index,1)
         console.log("denna todo låg på på index " +index)
-        if (myList.done===false){
-            myList.done=true;
-            console.log(myList.done)
-            return myList;
+        if (clickedItem.done===false){
+            clickedItem.done=true;
+            //console.log(clickedItem.done) 
+            console.log(clickedItem) //här är done:true
+            return clickedItem;
             
         }
     }
     else {
-
+        
     }
-    console.log(myList)
+    
 }
+
 
