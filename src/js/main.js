318110df296051e2fbdc3f6ff8c6410c91a4951f
Mira <miravitsas1@gmail.com>
2022-11-03 11:38:31 +0100

    input fixad

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,31 @@
+import { Todo } from "./models/Todolist";
+
+
 window.onload = function() {
     createList();
     myButton();
+    sortButton();
     console.log(toDoList);   
   }
-  
-  class Todo {
-    constructor(things,date,done){
-        this.things=things;
-        this.date=date;
-        this.done=done;
-    }      
-    }
   
   let toDoList = 
   [new Todo("äta",new Date(),false)
   ,new Todo("gymma",new Date(),false)
   ,new Todo("plugga",new Date(),false)
   ,new Todo("sova",new Date(),false)
-  ,new Todo("städa",new Date(),false),]
-  
+  ,new Todo("städa",new Date(),false)
+]
   let toDoListChecked = [];
-  
-  //myList = toDoList;
-  
+
+ 
   function createList(){
   let myDiv = document.getElementById("listdiv");
   let newUl = document.getElementById("myUl");
   myDiv.className="container";
   newUl.className="todoUl";
   myDiv.appendChild(newUl);
-  
+  newUl.innerHTML= " ";
+
   for (i=0;i<toDoList.length;i++){
     let newLi = document.createElement("li");
     newLi.className="todoList"
@@ -44,9 +40,6 @@ window.onload = function() {
     newLi.addEventListener("click", ()=>{ 
         myCheckbox(newInput, newLi, theList, )}); //när jag klickar skickas dessa 3 värden till min function myCheckBox()              
   } 
-
-
-  
   //console.log(toDoList) //skriver mina objekt. Här syns även att done:false
   
   function myCheckbox(myInput, newLi, clickedItem,) {
@@ -75,18 +68,17 @@ window.onload = function() {
             checkedli.addEventListener("click", ()=>{ 
                 goBack(checkedli, newLi, myValue, checkedInput,clickedItem)});
                 
-                 
+                 console.log(clickedItem) //done:true när jag klikat
                 if (clickedItem.done===true) {
                    let changeToFalse = clickedItem.done=false
                     toDoListChecked.push(changeToFalse.things)
                      //här pushar till min lista som hetere toDoListChecked så att mina gjorda saker ska samlas i en egen lista                       
         }
-   
      
   }
   function goBack (newCheckedLi, newLi, theValue, checkedInput, clickedItem) { //här försöker jag göra tt klickevent så att ifall de gjorda chheckboxarna klickas i ska de åka tillbaka till ursprungslistan 
         toDoList.push(clickedItem)
-        toDoListChecked.pop() 
+        //toDoListChecked.pop() 
         newCheckedLi.innerHTML=" "; 
         newLi.innerHTML=theValue;
         newLi.appendChild(checkedInput);
@@ -94,7 +86,7 @@ window.onload = function() {
        
   
   }
-  //console.log(toDoListChecked) //Nya listan uppdaterad med sakerna som är gjorda
+  //Nya listan uppdaterad med sakerna som är gjorda
   console.log("vad som finns kvar i listan: ",toDoList) 
   }
   }
@@ -112,11 +104,16 @@ window.onload = function() {
   function addToList() { //sparar value från input och gör om den till false. Vet dock inte hur jag ska få in den i min klass.
     let inputValue = document.getElementById("userForm").value;
     //console.log(inputValue)
-
-
     BackToList = new Todo(inputValue,new Date(),false)
     toDoList.push(BackToList);
     console.log(BackToList) //Här ser jag att det jag skrivit blir ett objekt som matchar min lista.nu måste jag bara skicka in denna i min lista.
     createList()//nu loopas hela listan igen + nya värdet. vill bara få ut nya värdet. Bhövr jag göra en ny loop kanske?
   }
   
+function sortButton() {
+
+}
+
+function sortList () {
+
+}
---
+++ src/js/models/Todolist.js
@@ -0,0 +1,7 @@
+export class Todo {
+    constructor(things,date,done){
+        this.things=things;
+        this.date=date;
+        this.done=done;
+    }      
+    }
